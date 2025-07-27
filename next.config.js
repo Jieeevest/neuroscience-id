3a45e8ce96322108639bee351e849b097512ef15
Iqbaalaji <ajieeevest@gmail.com>
2025-07-28 00:26:25 +0700

    adjust next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
-    domains: ['img.youtube.com', 'graph.facebook.com'],
+    domains: ["img.youtube.com", "graph.facebook.com"],
   },
 };
 
